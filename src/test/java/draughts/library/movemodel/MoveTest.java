788d5f63abddbdfc32fc44a882b13bb375803696
krystian-rymon-lipinski <rymon9696@gmail.com>
2020-10-17 17:56:47 +0200

    Test classifying moves

--- src/test/java/draughts/library/movemodel/MoveTest.java
+++ src/test/java/draughts/library/movemodel/MoveTest.java
@@ -1,7 +1,5 @@
 package draughts.library.movemodel;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,6 +13,8 @@ import draughts.library.managers.BoardManager;
 import draughts.library.movemodel.Capture;
 import draughts.library.movemodel.Hop;
 import draughts.library.movemodel.Move;
+
+import static org.junit.Assert.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MoveTest {
@@ -85,6 +85,52 @@ public class MoveTest {
 		assertEquals(4, testObj2.getMoveDestination().getIndex());
 		assertEquals(3, testObj2.getMoveTakenPawns().size());
 		assertEquals(whitePawn3, testObj2.getMoveTakenPawns().get(2));
+	}
+
+	@Test
+	public void classify_noPromotion_noCapture() {
+		Piece whitePawn = new WhitePawn(getTile(20));
+
+		testObj = new Move<>(whitePawn, new Hop(getTile(20), getTile(15)));
+		testObj.classify();
+
+		assertFalse(testObj.isCapture());
+		assertFalse(testObj.isPromotion());
+	}
+
+	@Test
+	public void classify_noPromotion_capture() {
+		Piece blackPawn = new BlackPawn(getTile(33));
+		Piece whitePawn = new WhitePawn(getTile(28));
+
+		testObj2 = new Move<>(blackPawn, new Capture(getTile(33), getTile(22), whitePawn));
+		testObj2.classify();
+
+		assertFalse(testObj2.isPromotion());
+		assertTrue(testObj2.isCapture());
+	}
+
+	@Test
+	public void classify_promotion_noCapture() {
+		Piece whitePawn = new WhitePawn(getTile(9));
+
+		testObj = new Move<>(whitePawn, new Hop(getTile(9), getTile(3)));
+		testObj.classify();
+
+		assertTrue(testObj.isPromotion());
+		assertFalse(testObj.isCapture());
+	}
+
+	@Test
+	public void classify_promotion_capture() {
+		Piece blackPawn = new BlackPawn(getTile(37));
+		Piece whitePawn = new WhitePawn(getTile(42));
+
+		testObj2 = new Move<>(blackPawn, new Capture(getTile(37), getTile(48), whitePawn));
+		testObj2.classify();
+
+		assertTrue(testObj2.isPromotion());
+		assertTrue(testObj2.isCapture());
 	}
 
 }
